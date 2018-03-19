085654d989f1acb1a6f89b54f988554648397583
YukiOta <yuki.ota@ruri.waseda.jp>
2018-03-20 02:11:54 +0900

    update 2018/3/20

---
+++ makelinks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ln -fnsv ~/dotfiles/.vimrc ~/.vimrc
+ln -fnsv ~/dotfiles/.vim/colors ~/.vim/colors
+ln -fnsv ~/dotfiles/.zshrc ~/.zshrc
+ln -fnsv ~/dotfiles/.zshenv ~/.zshenv
+ln -fnsv ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+ln -fnsv ~/dotfiles/.vimrc ~/.vim/init.vim
+ln -fnsv ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+ln -fnsv ~/dotfiles/.vim ~/.config/nvim
