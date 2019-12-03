41e045ff3734ec385b5b480a761ef06524ff28a6
Yuki Ota <yuki.ota@ruri.waseda.jp>
2019-12-03 11:55:41 +0900

    add nodebrew path

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -1,7 +1,7 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/usr/local/texlive/2017/bin/x86_64-darwin:/opt/X11/bin
 
 # nodebrew
-# export PATH=$HOME/.nodebrew/current/bin:$PATH
+export PATH=$HOME/.nodebrew/current/bin:$PATH
 
 # ローカルインストールしたpackageを実行するためのpath
 export PATH=$PATH:./node_modules/.bin
